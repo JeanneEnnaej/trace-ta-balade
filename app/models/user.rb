7d93c70e07048bdb6eb4b1f692d63b91c8ebebdf
jeanne desmier <desmierjeanne@gmail.com>
2022-09-16 09:58:56 +0200

    validations des models et relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  has_many :walks
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, :last_name, presence: true
 end
