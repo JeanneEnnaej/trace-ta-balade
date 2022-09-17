e049ade18d9fe26100c451875ed25f8a6e79718a
jeanne desmier <desmierjeanne@gmail.com>
2022-09-17 18:35:17 +0200

    creation des views, du form et d'une partie du controller walk

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -1,2 +1,34 @@
 class WalksController < ApplicationController
+  before_action :set_walk, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @walks = Walk.where(user: current_user)
+  end
+
+  def show
+  end
+
+  def new
+    @walk = Walk.new
+  end
+
+  def create
+    @walk = Walk.new(walk_params)
+    if @walk.save
+      redirect_to walk_path(@walk)
+    else
+      render "form_walk", status: :unprocessable_entity
+    end
+  end
+
+
+  private
+
+  def walk_params
+    params.require(:walk).permit(:title, :num_km ,:duration, :ratting, :address, :date, :content, :disadvantages, :advantages, photos: [] )
+  end
+
+  def set_walk
+    @walk = Walk.find(:id)
+  end
 end
