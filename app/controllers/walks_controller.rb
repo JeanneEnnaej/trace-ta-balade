c8249b77a9dbf4974cef5e09d4537a568c04bc1a
jeanne desmier <desmierjeanne@gmail.com>
2022-09-17 19:24:48 +0200

    problème de sauvegarde de la walk, voir avec nouvelle db

--- app/controllers/walks_controller.rb
+++ app/controllers/walks_controller.rb
@@ -13,19 +13,19 @@ class WalksController < ApplicationController
   end
 
   def create
-    @walk = Walk.new(walk_params)
-    if @walk.save
-      redirect_to walk_path(@walk)
-    else
-      render "form_walk", status: :unprocessable_entity
-    end
+    @walk = Walk.create!(walk_params)
+    # if @walk.save!
+    #   redirect_to walk_path(@walk)
+    # else
+    #   render "form_walk", status: :unprocessable_entity
+    # end
   end
 
 
   private
 
   def walk_params
-    params.require(:walk).permit(:title, :num_km ,:duration, :ratting, :address, :date, :content, :disadvantages, :advantages, photos: [] )
+    params.require(:walk).permit(:title, :num_km ,:duration, :ratting, :address, :date, :content, :user_id, :disadvantages, :advantages, photos: [])
   end
 
   def set_walk
