18f7f449a05c82443c1e4f255f9fc3c476ecff86
jeanne desmier <desmierjeanne@gmail.com>
2022-09-18 17:05:17 +0200

    relations entre les models et nouvelles tables de jointures

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -1,5 +1,7 @@
 class Walk < ApplicationRecord
   belongs_to :user
+  has_many :advantages, through: :walk_advantage
+  has_many :disadvantages, through: :walk_disadvantage
   has_many_attached :photos
 
   validates :title, :address, :date, presence: true
