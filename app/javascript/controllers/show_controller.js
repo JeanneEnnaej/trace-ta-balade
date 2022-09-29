3acbcefdd6370464a4afbaf48c0c36cd5825c813
jeanne desmier <desmierjeanne@gmail.com>
2022-09-29 16:43:12 +0200

    début de la pop up show

---
+++ app/javascript/controllers/show_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["popup"]
+  static values = {
+    id: String,
+  }
+
+  connect() {
+    console.log('coucou')
+  }
+
+  showPopup() {
+    this.popupTarget.classList.toggle("display-none");
+  }
+}
