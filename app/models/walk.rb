923ba39c7c726f08c6c0b7f2ae82aa70b0d8f556
jeanne desmier <desmierjeanne@gmail.com>
2022-10-28 11:22:42 +0200

    test validates pour les photos

--- app/models/walk.rb
+++ app/models/walk.rb
@@ -9,4 +9,16 @@ class Walk < ApplicationRecord
   validates :title, :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  # validate :image_size_validation
+  validate :validate_images
+
+  # def image_size_validation
+  #   errors[:photos] << "Les photos sont trop lourdes, >10Mo" if photos.size > 80.megabytes
+  # end
+
+  def validate_images
+    return if photos.count <= 5
+
+    errors.add(:images, 'Tu ne peux ajouter que 5 photos')
+  end
 end
