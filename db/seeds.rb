fa904f5034c145731debfb4bb7bff3f3b25a439c
jeanne desmier <desmierjeanne@gmail.com>
2022-11-22 16:39:39 +0100

    changement texte temoignage

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,9 +27,8 @@ disadvantages.each do |d|
 end
 
 p "creation des temoignages"
-reviews = ["En politique, mon cher, vous le savez comme moi, il n'y a pas d'hommes, mais des idées ;
-  pas de sentiments, mais des intérêts ; en politique, on ne tue pas un homme : on supprime un obstacle, voilà tout.","Apprendre n'est pas savoir ; il y a les sachant et les savants : c'est la mémoire qui fait les uns, c'est le philosophie qui fait les autres.
-  - Mais ne peut-on apprendre la philosophie ?","La philosophie ne s'apprend pas ; la philosophie est la réunion des sciences acquises au génie qui les applique : la philosophie, c'est le nuage éclatant sur lequel le Christ a posé le pied pour remonter au ciel." ]
+reviews = ["Plus besoin d'ouvrir 2à onglets différents sur pleins de sites pour choisir sa balade",
+  "J'aime la possibilité de joindre un lien externe", "Vivement les nouvelles fonctionnalités !"]
 
 reviews.each do |r|
   Review.create(
