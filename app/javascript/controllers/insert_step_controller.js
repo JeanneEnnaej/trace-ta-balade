dd041e82d934077cbc06d18faf38fc7d9896f3b5
jeanne desmier <desmierjeanne@gmail.com>
2022-12-29 10:02:07 +0100

    close this branch to save this part of code

--- app/javascript/controllers/insert_step_controller.js
+++ app/javascript/controllers/insert_step_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import { node } from "webpack"
+
 
 // Connects to data-controller="insert-step"
 export default class extends Controller {
@@ -13,21 +13,6 @@ export default class extends Controller {
 
   add(event) {
     event.preventDefault()
-    console.log(this.itemTarget)
-    // const node = document.createElement("p");
-    // const textnode = document.createTextNode(this.itemTarget.outerHTML);
-    // node.appendChild(textnode);
-    // document.getElementById("myList").appendChild(node);
-
-    // const node2 = document.createElement("p");
-    // const textnode2 = document.createTextNode("Nom de l'étape :");
-    // node2.appendChild(textnode2);
-    // document.getElementById("myList").appendChild(node2);
-    // const node3 = document.createElement("input");
-    // node3.setAttribute("type", "text");
-    // node3.setAttribute("name", "step[name]");
-    // node3.setAttribute("placeholder", "Nom de l'étape");
-    // document.getElementById("myList").appendChild(node3);
 
   }
 }
