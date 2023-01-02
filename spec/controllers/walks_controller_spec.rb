48800492cf4810441eec84d9f59ef4035db6ead8
jeanne desmier <desmierjeanne@gmail.com>
2023-01-02 10:34:59 +0100

    Mock geocoder

--- spec/controllers/walks_controller_spec.rb
+++ spec/controllers/walks_controller_spec.rb
@@ -4,6 +4,15 @@ RSpec.describe WalksController, type: :controller do
 
   describe "GET #index" do
     login_user
+    before(:each) do
+      Geocoder.configure(:lookup => :test)
+      Geocoder::Lookup::Test.add_stub(
+        "01700 Miribel, France", [
+          { 'latitude'     => 45.8235227,
+            'longitude'    => 4.9528525,
+          }
+          ])
+    end
 
     it "Index walks" do
       get :index
@@ -17,8 +26,18 @@ RSpec.describe WalksController, type: :controller do
   end
 
   describe "GET #show" do
+    before(:each) do
+      Geocoder.configure(:lookup => :test)
+      Geocoder::Lookup::Test.add_stub(
+        "01700 Miribel, France", [
+          { 'latitude'     => 45.8235227,
+            'longitude'    => 4.9528525,
+          }
+          ])
+    end
     login_user
     walk = FactoryBot.create(:walk)
+
     it "Show walk" do
       get :show, params: { id: walk.id }
       assert_response :success
@@ -31,16 +50,25 @@ RSpec.describe WalksController, type: :controller do
   end
 
   describe "POST #create" do
+    before(:each) do
+      Geocoder.configure(:lookup => :test)
+      Geocoder::Lookup::Test.add_stub(
+        "01700 Miribel, France", [
+          { 'latitude'     => 45.8235227,
+            'longitude'    => 4.9528525,
+          }
+          ])
+    end
     login_user
 
     it "Create walk" do
       expect {
-        post :create, params: { walk: { title: "Test Walk", address: "123 rue leon blum", user_id: subject.current_user.id } }
+        post :create, params: { walk: { title: "Test Walk", address: "01700 Miribel, France", user_id: subject.current_user.id } }
       }.to change(Walk, :count).by(1)
     end
 
     it "redirects to the new walk" do
-      post :create, params: { walk: { title: "Test Walk", address: "123 rue leon blum", user_id: subject.current_user.id } }
+      post :create, params: { walk: { title: "Test Walk", address: "01700 Miribel, France", user_id: subject.current_user.id } }
       expect(response).to redirect_to(Walk.last)
     end
 
@@ -54,9 +82,18 @@ RSpec.describe WalksController, type: :controller do
   describe "PATCH #update" do
     login_user
     walk = FactoryBot.create(:walk)
+    before(:each) do
+      Geocoder.configure(:lookup => :test)
+      Geocoder::Lookup::Test.add_stub(
+        "01700 Miribel, France", [
+          { 'latitude'     => 45.8235227,
+            'longitude'    => 4.9528525,
+          }
+          ])
+    end
 
     it "Update walk" do
-      patch :update, params: { id: walk.id, walk: { title: "Test Walk", address: "123 rue leon blum", user_id: subject.current_user.id } }
+      patch :update, params: { id: walk.id, walk: { title: "Test Walk", address: "01700 Miribel, France", user_id: subject.current_user.id } }
       expect(response).to redirect_to(walk)
     end
 
@@ -69,6 +106,15 @@ RSpec.describe WalksController, type: :controller do
   describe "DELETE #destroy" do
     login_user
     walk = FactoryBot.create(:walk)
+    before(:each) do
+      Geocoder.configure(:lookup => :test)
+      Geocoder::Lookup::Test.add_stub(
+        "01700 Miribel, France", [
+          { 'latitude'     => 45.8235227,
+            'longitude'    => 4.9528525,
+          }
+          ])
+    end
 
     it "Delete walk" do
       expect {
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     first_name { "Jeanne" }
     last_name { "D'Arc" }
-    address { "123 Main St" }
     email { Faker::Internet.email }
     password { "password123" }
     password_confirmation { "password123" }
