8127d4e6d983497b68a27479f7f35a70d7a8acd5
jeanne desmier <desmierjeanne@gmail.com>
2023-01-02 09:50:45 +0100

    Tests steps controller, index, create, update, delete

--- spec/controllers/walks_controller_spec.rb
+++ spec/controllers/walks_controller_spec.rb
@@ -2,83 +2,83 @@ require 'rails_helper'
 
 RSpec.describe WalksController, type: :controller do
 
-  # describe "GET #index" do
-  #   login_user
+  describe "GET #index" do
+    login_user
 
-  #   it "Index walks" do
-  #     get :index
-  #     assert_response :success
-  #   end
+    it "Index walks" do
+      get :index
+      assert_response :success
+    end
 
-  #   it "renders the index template" do
-  #     get :index
-  #     expect(response).to render_template("index")
-  #   end
-  # end
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
 
-  # describe "GET #show" do
-  #   login_user
-  #   walk = FactoryBot.create(:walk)
-  #   it "Show walk" do
-  #     get :show, params: { id: walk.id }
-  #     assert_response :success
-  #   end
+  describe "GET #show" do
+    login_user
+    walk = FactoryBot.create(:walk)
+    it "Show walk" do
+      get :show, params: { id: walk.id }
+      assert_response :success
+    end
 
-  #   it "renders the show template" do
-  #     get :show, params: { id: walk.id }
-  #     expect(response).to render_template("show")
-  #   end
-  # end
+    it "renders the show template" do
+      get :show, params: { id: walk.id }
+      expect(response).to render_template("show")
+    end
+  end
 
-  # describe "POST #create" do
-  #   login_user
+  describe "POST #create" do
+    login_user
 
-  #   it "Create walk" do
-  #     expect {
-  #       post :create, params: { walk: { title: "Test Walk", address: "123 rue leon blum", user_id: subject.current_user.id } }
-  #     }.to change(Walk, :count).by(1)
-  #   end
+    it "Create walk" do
+      expect {
+        post :create, params: { walk: { title: "Test Walk", address: "123 rue leon blum", user_id: subject.current_user.id } }
+      }.to change(Walk, :count).by(1)
+    end
 
-  #   it "redirects to the new walk" do
-  #     post :create, params: { walk: { title: "Test Walk", address: "123 rue leon blum", user_id: subject.current_user.id } }
-  #     expect(response).to redirect_to(Walk.last)
-  #   end
+    it "redirects to the new walk" do
+      post :create, params: { walk: { title: "Test Walk", address: "123 rue leon blum", user_id: subject.current_user.id } }
+      expect(response).to redirect_to(Walk.last)
+    end
 
-  #   it "Does not create walk" do
-  #     expect {
-  #       post :create, params: { walk: { title: "", address: "", user_id: subject.current_user.id } }
-  #     }.to change(Walk, :count).by(0)
-  #   end
-  # end
+    it "Does not create walk" do
+      expect {
+        post :create, params: { walk: { title: "", address: "", user_id: subject.current_user.id } }
+      }.to change(Walk, :count).by(0)
+    end
+  end
 
-  # describe "PATCH #update" do
-  #   login_user
-  #   walk = FactoryBot.create(:walk)
+  describe "PATCH #update" do
+    login_user
+    walk = FactoryBot.create(:walk)
 
-  #   it "Update walk" do
-  #     patch :update, params: { id: walk.id, walk: { title: "Test Walk", address: "123 rue leon blum", user_id: subject.current_user.id } }
-  #     expect(response).to redirect_to(walk)
-  #   end
+    it "Update walk" do
+      patch :update, params: { id: walk.id, walk: { title: "Test Walk", address: "123 rue leon blum", user_id: subject.current_user.id } }
+      expect(response).to redirect_to(walk)
+    end
 
-  #   it "Does not update walk" do
-  #     patch :update, params: { id: walk.id, walk: { title: "", address: "", user_id: subject.current_user.id } }
-  #     expect(response).to redirect_to(edit_walk_path(walk))
-  #   end
-  # end
+    it "Does not update walk" do
+      patch :update, params: { id: walk.id, walk: { title: "", address: "", user_id: subject.current_user.id } }
+      expect(response).to redirect_to(edit_walk_path(walk))
+    end
+  end
 
-  # describe "DELETE #destroy" do
-  #   login_user
-  #   walk = FactoryBot.create(:walk)
+  describe "DELETE #destroy" do
+    login_user
+    walk = FactoryBot.create(:walk)
 
-  #   it "Delete walk" do
-  #     expect {
-  #       delete :destroy, params: { id: walk.id }
-  #     }.to change(Walk, :count).by(-1)
-  #   end
+    it "Delete walk" do
+      expect {
+        delete :destroy, params: { id: walk.id }
+      }.to change(Walk, :count).by(-1)
+    end
 
-  #   it "redirects to walks index" do
-  #     delete :destroy, params: { id: walk.id }
-  #     expect(response).to redirect_to(walks_path)
-  #   end
-  # end
+    it "redirects to walks index" do
+      delete :destroy, params: { id: walk.id }
+      expect(response).to redirect_to(walks_path)
+    end
+  end
 end
